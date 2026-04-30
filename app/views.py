4eb3b30611aaf6b24ed470992d8d6c0b6c11b7b7
Stephen-Jon Dixon <stephenjondixon1@gmail.com>
2026-04-29 22:48:02 -0500

    Added updated files for lab submission

--- app/views.py
+++ app/views.py
@@ -1,41 +1,48 @@
 from app import app
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template
+import datetime
 
-
-###
-# Routing for your application.
-###
 
 @app.route('/')
 def home():
-    """Render website's home page."""
     return render_template('home.html')
 
 
 @app.route('/about/')
 def about():
-    """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
 
-###
-# The functions below should be applicable to all Flask apps.
-###
+def format_date_joined(date_joined):
+    return date_joined.strftime("%B, %Y")
+
+
+@app.route('/profile')
+def profile():
+    date_joined = datetime.date(2026, 2, 7)
+
+    user = {
+        "full_name": "Stephen-Jon Dixon",
+        "username": "stephen",
+        "location": "Kingston, Jamaica",
+        "date_joined": format_date_joined(date_joined),
+        "bio": "I am a web development student (again) who wants to start doing better in life with myself and taking things more seriously.",
+        "posts": 7,
+        "following": 100,
+        "followers": 250
+    }
+
+    return render_template('profile.html', user=user)
+
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
-    """Send your static text file."""
     file_dot_text = file_name + '.txt'
     return app.send_static_file(file_dot_text)
 
 
 @app.after_request
 def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also tell the browser not to cache the rendered page. If we wanted
-    to we could change max-age to 600 seconds which would be 10 minutes.
-    """
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=0'
     return response
@@ -43,5 +50,4 @@ def add_header(response):
 
 @app.errorhandler(404)
 def page_not_found(error):
-    """Custom 404 page."""
     return render_template('404.html'), 404
